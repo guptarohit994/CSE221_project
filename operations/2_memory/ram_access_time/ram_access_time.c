ce2c53e1129c4bf6a62b1a53b57ce2dd16a066f6
albus <guptarohit994@gmail.com>
2019-10-29 11:13:04 -0700

    Fixes to LL creation

--- operations/2_memory/ram_access_time/ram_access_time.c
+++ operations/2_memory/ram_access_time/ram_access_time.c
@@ -63,12 +63,12 @@ void do_accesses(Node *n, size_t mem_size)
     }else{
         stride = n - prev_node;
     }
-    //printf("n:%p, prev_node:%p\n", n, prev_node);
-    printf("data[%d]:0x%X, stride:%zuK, AccessTime:%zu cycles\n", data_to_access,n->data[data_to_access], stride, access_time);
+    //printf("n:%zu, prev_node:%zu\n", n->id, prev_node->id);
+    //printf("data[%d]:0x%X, stride:%zuK, AccessTime:%zu cycles\n", data_to_access,n->data[data_to_access], stride, access_time);
     prev_node = n;
     n = n->next; 
   } 
-  printf("total_linked_list_nodes:%zu, mem_size:%zuK, averageAccessTime:%0.2f cyles\n", count_nodes,mem_size>>10, (double)total_access_time/count_nodes);
+  printf("total_linked_list_nodes:%zu/%zu, mem_size:%zuB, averageAccessTime:%0.2f cycles\n", count_nodes, (size_t)(mem_size / sizeof(Node)), mem_size, (double)total_access_time/count_nodes);
 }
 
 /* this function creates a linked list either in sequential or non-sequential manner
@@ -78,7 +78,7 @@ Node * prepare_linked_list (size_t memory_region_size){
   //printf("preparing memory of size:%zuK\n", memory_region_size>>10);
 
   size_t num_nodes_possible = (size_t) (memory_region_size / sizeof(Node));
-  printf("num_nodes_possible:%zu\n", num_nodes_possible);
+  //printf("num_nodes_possible:%zu\n", num_nodes_possible);
 
   array = (Node*) malloc(sizeof(Node) * num_nodes_possible);
   memset(array, 0x00, memory_region_size);
@@ -96,8 +96,7 @@ Node * prepare_linked_list (size_t memory_region_size){
   }
 
   //setup linked list
-  size_t max_count = num_nodes_possible;
-  size_t random_offset = 1;
+  size_t random_offset = 0;
   size_t previous_offset = 0;
   int rand_nums_occurred[num_nodes_possible];
 
@@ -106,20 +105,22 @@ Node * prepare_linked_list (size_t memory_region_size){
   }
   rand_nums_occurred[0] = 1;
     
-  for (i = 0; i < num_nodes_possible - 1; i++){
+  for (i = 0; i < num_nodes_possible; i++){
 
 #ifndef SEQUENTIAL_ACCESS
     int repeat = 1;
-    int count = 0;
-    while (repeat == 1 && count < 300){
+    size_t count = 0;
+    size_t max_count = 10*i;
+    while (repeat == 1 && count < max_count){
       random_offset += rand();
       random_offset %= num_nodes_possible;
+      //printf("checking for random_offset:%zu\n", random_offset);
       if (rand_nums_occurred[random_offset] == 1){
         repeat = 1;
         count++;
-        //printf("count:%d\n",count );
-        if (count >= 1000){
-          printf("Failed to select a node even after 1000 tries!\n");
+        //printf("count:%zu\n",count );
+        if (count >= max_count){
+          printf("Failed to select a node even after %zu tries!\n", max_count);
           exit(1);
         }
       } else{
@@ -146,6 +147,8 @@ Node * prepare_linked_list (size_t memory_region_size){
 
 /* main function to setup the linked list and start multi-threaded execution */
 int main(int argc, char **argv) {
+  set_nice(-20);
+
   struct Node *array;
   int i, rc;
 
@@ -163,31 +166,33 @@ int main(int argc, char **argv) {
      29<<20, 30<<20, 31<<20, 32<<20, 64<<20,128<<20,256<<20,512<<20,1024<<20,
     };
   int l;
-  for (l=16; l<17; l++){
-    printf("-----------------------------------------------------------------\n");
+  for (l=0; l<75; l++){
+    //printf("-----------------------------------------------------------------\n");
     //printf("l:%d\n",l);
     mem_size = temp_size[l];
   //for (mem_size = 512; mem_size <= 1024*1024*1024; mem_size = mem_size*2){
     //printf("mem_size:%zuK\n", mem_size>>10);
     array = prepare_linked_list(mem_size);
 
-    //do_accesses(array, mem_size);
     
-    Node* current = array;
-    Node* next = array;
+    
+    // Node* current = array;
+    // Node* next = array;
 
 
-    //printf("->(%zu) ", current->id);
-    while (next != NULL){
-      printf("->(%zu) ", current->id);
-      next = current->next;
-      //printf("current:%p, next:%p\n", current, next);
-      current = next;
-    }
-    printf("\n");
+    // //printf("->(%zu) ", current->id);
+    // while (next != NULL){
+    //   printf("->(%zu) ", current->id);
+    //   next = current->next;
+    //   //printf("current:%p, next:%p\n", current, next);
+    //   current = next;
+    // }
+    // printf("\n");
+
+    do_accesses(array, mem_size);
 
     //printf("Main_thread: hello, successfully completed for mem_size:%zuK\n", mem_size>>10);
-    printf("-----------------------------------------------------------------\n");
+    //printf("-----------------------------------------------------------------\n");
     free(array);
   }
   
