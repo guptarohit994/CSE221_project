33daa98bae9898e05a204d61889c26725049604e
gigaflw <a_gigantic_flower@yeah.net>
2019-11-11 21:10:14 -0800

    Updating page_fault_time result

--- operations/2_memory/page_fault_time/page_fault_time.c
+++ operations/2_memory/page_fault_time/page_fault_time.c
@@ -3,19 +3,20 @@
 #include <math.h>
 #include "../../../utils/utils.h"
 
-#define NUM_ITERATIONS 10000
-#define PAGE_SIZE 4096
-#define MAX_N_PAGES 20
+#define PAGE_SIZE 4096 // 4 bytes
+#define PAGE_READ_OFFSET 16<<20 // 16 MB
+#define FILE_SIZE 1<<30 // 1 GB
 #define FAKE_FILE "build/temp_1GB_file"
 
 struct MMem { 
   int fd;
 
   char* ptr;
-  int size;
-  int npages;
+  int faulted_data;
+  int nreads;
   int page_size;
-  size_t* pages;
+  int file_size;
+  int offset;
 };
 
 // This permutation code is taken from lmbench3
@@ -27,6 +28,7 @@ struct MMem {
 //  // 10 is the unit in perm10, e.g. [30, 10, 0, 20]
 //  free(perm);
 //  free(perm10); // remember to free it
+// Note: as a result of experiments, permutation doesn't make a significant difference
 size_t* permutation(int max, int scale) {
     size_t  i, v;
     static size_t r = 0;
@@ -52,7 +54,7 @@ size_t* permutation(int max, int scale) {
 #ifdef _DEBUG
     fprintf(stderr, "permutation(%d): {", max);
     for (i = 0; i < max; ++i) {
-      fprintf(stderr, "%d", result[i]);
+      fprintf(stderr, "%zu", result[i]);
       if (i < max - 1) 
         fprintf(stderr, ",");
     }
@@ -106,56 +108,78 @@ struct linear_regression_result_t linear_regression(double* xs, double* ys, int 
 }
 
 // Benchmark for page faults
-// In each iteration, `mmem->npages` page faults are triggered
-// NUM_ITERATIONS iterations in total
+// In each call, `mmem->nreads` page faults are triggered
 uint64_t benchmark(struct MMem* mmem) {
     struct Timer timer;
     char poker = 0;
 
+     // sync: complete all disk writes, purge: rest disk caches
+    if (system("sync") == -1) handle_error_en(-1, "sync");
+    if (system("purge") == -1) handle_error_en(-1, "purge");
+
+    mmem->ptr = (char*)mmap(0, mmem->file_size, PROT_READ, MAP_SHARED, mmem->fd, 0);
     tic(timer);
-    for (int i=0; i < NUM_ITERATIONS; ++i) {
-        mmem->ptr = (char*)mmap(0, mmem->size, PROT_READ, MAP_SHARED, mmem->fd, 0);
-        for (int j = 0; j < mmem->npages; ++j) {
-            poker = *((mmem->ptr + mmem->pages[j]));
-        }
-        munmap(mmem->ptr, mmem->size);
+    for (uint64_t j = 0; j < mmem->nreads; ++j) {
+        // printf("off=%llu\n", ((j * mmem->offset) % mmem->file_size));
+        poker = mmem->ptr[(j * mmem->offset) % mmem->file_size];
     }
     toc(timer);
-    return (uint64_t) (timer_diff(timer));
+    munmap(mmem->ptr, mmem->file_size);
+
+    return timer_diff(timer);
 }
 
-
 int main() {
-    struct MMem mmem;
-    mmem.fd = open(FAKE_FILE, O_RDONLY);
+    set_nice(-20);
 
+    struct MMem mmem;
+    mmem.page_size = PAGE_SIZE;
+    mmem.file_size = FILE_SIZE;
+    mmem.offset = PAGE_READ_OFFSET;
+
+    // prepare file
+    mmem.fd = open(FAKE_FILE, O_RDONLY);
     if (mmem.fd <= 0)
         handle_error("No file named " FAKE_FILE " found");
 
-    mmem.page_size = PAGE_SIZE;
+    const int nreads[] = {1, 10, 20, 30, 40, 50};
+    const int nruns = sizeof(nreads) / sizeof(int);
+    double sizes[nruns], overheads[nruns];
 
-    double sizes[MAX_N_PAGES], overheads[MAX_N_PAGES];
-
-    for (int i = 0; i < MAX_N_PAGES; ++i) {
-        mmem.size = PAGE_SIZE * (i+1);
-        mmem.npages = 1 + (mmem.size - 1) / mmem.page_size;
-        mmem.pages = permutation(mmem.npages, mmem.page_size);
+    for (int i = 0; i < nruns; ++i) {
+        mmem.nreads = nreads[i];
+        mmem.faulted_data = PAGE_SIZE * nreads[i];
         uint64_t cycles = benchmark(&mmem);
-        free(mmem.pages);
-        printf("%d, %llu\n", mmem.size, cycles);
 
-        if ((int) cycles != cycles) {
-            handle_error("time overflow");
-        }
-
-        sizes[i] = (double) (mmem.size >> 10); // unit: 1KB
-        overheads[i] = (double) (cycles/1e6);  // unit: 1M cycles
+#ifdef _DEBUG
+        printf("%d, %llu\n", mmem.faulted_data, cycles);
+#endif
+        sizes[i] = (double) (mmem.faulted_data >> 10); // unit: 1KB
+        overheads[i] = ((double)cycles/1e3);  // unit: 1K cycles
     }
 
     close(mmem.fd);
 
-    struct linear_regression_result_t reg = linear_regression(sizes, overheads, MAX_N_PAGES);
-    printf("linear fit: overhead = %.2f size (KB) + %.2f (Mcycles), max_diff=%.2f\n", reg.slope, reg.intercept, reg.max_diff);
+    struct linear_regression_result_t reg = linear_regression(sizes, overheads, nruns);
+
+#ifdef _DEBUG
+    printf("linear fit: overhead = %.5f size (KB) + %.5f (Kcycles), max_diff=%.5f\n", reg.slope, reg.intercept, reg.max_diff);
+#endif
+    // reg.slope: Kcycles/KB
+    // bandwidth: MB/s = MB/KB * KB/Kcycles * Kcycles/cycles * cycles/s
+    //                 = 1/1024 * 1/reg.slope * 1/1e3 * 2.7e9
+    // page fault service time (ns) = PAGE_SIZE / bandwidth * 1e9
+    // page fault service time: cycles/page
+    //  = Kcycles/KB * KB/page * cycles/Kcycles
+    //  = reg.slope * PAGE_SIZE/1024 * 1K
+    double service_time = reg.slope * 1e3 * PAGE_SIZE / 1024.0;
+    cnprintf(LOW, "main", "\n\n");
+    cnprintf(LOW, "main", "***************** RESULT *****************");
+    cnprintfsi(LOW, "main", "cycles_taken", (int) service_time);
+#ifdef _DEBUG
+    double bandwidth = 2.7e6/1024/reg.slope;
+    printf("%.9f (ms/page), bandwidth=%.2f(MB/s)\n", service_time/2.7e6, bandwidth);
+#endif
 }
 
 // Note: Without permutation, overhead is constant 
